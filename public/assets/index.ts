425ec4a4eac95678161b35002662c53773b779c2
PurushottamKakde <kakdepurushottam683@gmail.com>
2025-06-25 01:49:44 +0530

    Update index.ts

--- public/assets/index.ts
+++ public/assets/index.ts
@@ -1,6 +1,6 @@
 import logo from "./images/logo.png";
 import profileImg from "./images/profile.jpg";
-import WanderLust from "./images/wanderLust.png";
+import WanderLust from "./images/WanderLust.png";
 import queryMinds from "./images/queryMinds.png";
 import gitShield from "./images/gitShield.png";
 import weatherSnap from "./images/weatherSnap.png";
